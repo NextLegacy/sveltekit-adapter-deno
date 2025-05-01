aa2c479c7a8c88196fa49a0cc1ae2ca639e70630
NextLegacy <mcserezli@gmail.com>
2025-05-01 19:29:35 +0200

    ✨ optimization

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,9 +2,10 @@ import type { Plugin } from "rollup";
 import type { Options } from "@swc/core";
 import type { MergedRollupOptions } from "rollup";
 import _swc from "@rollup/plugin-swc";
+import _nodeResolve, { type RollupNodeResolveOptions } from "@rollup/plugin-node-resolve";
 
 const swc = _swc as unknown as (options?: Options) => Plugin;
-
+const nodeResolve = _nodeResolve as unknown as (options?: RollupNodeResolveOptions) => Plugin;
 const rollup: MergedRollupOptions[] = [
     {
         input: "src/index.ts",
@@ -14,40 +15,12 @@ const rollup: MergedRollupOptions[] = [
                 format: "esm"
             }
         ],
-        plugins: [swc()],
-        external: ["@sveltejs/kit", "@sveltejs/kit/node", "MANIFEST", "@std/path", "SERVER", "node:process"]
+        plugins: [swc(), nodeResolve({
+            preferBuiltins: true,
+        })],
+        
+        external: ["@sveltejs/kit", "@sveltejs/kit/node", "MANIFEST", "SERVER"]
     },
-    {
-        input: "src/env.ts",
-        output: [
-            {
-                file: "build/env.ts",
-                format: "esm"
-            }
-        ],
-        plugins: [swc()]
-    },
-    {
-        input: "src/utils.ts",
-        output: [
-            {
-                file: "build/utils.ts",
-                format: "esm"
-            }
-        ],
-        plugins: [swc()]
-    },
-    {
-        input: "src/handler.ts",
-        output: [
-            {
-                file: "build/handler.ts",
-                format: "esm"
-            }
-        ],
-        plugins: [swc()],
-        external: ["@sveltejs/kit", "@sveltejs/kit/node", "MANIFEST", "@std/path", "SERVER"]
-    }
 ];
 
 export default rollup;
